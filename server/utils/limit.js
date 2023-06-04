cba8fabd8ea763ed14f6dc197f45ba9802944bda
Apacagnini <alejandro.pacagnini@gmail.com>
2023-06-04 09:51:04 -0300

    limit

--- server/utils/limit.js
+++ server/utils/limit.js
@@ -1,11 +1,16 @@
 const { searchModel } = require('../models/Search.model');
 const dotenv = require('dotenv');
 dotenv.config();
-const STORAGE_LIMIT = (process.env.STORAGE_LIMIT)? parseInt(process.env.STORAGE_LIMIT) : 300000;
+
+const STORAGE_LIMIT = (isNaN(parseInt(process.env.STORAGE_LIMIT)))? 0 : parseInt(process.env.STORAGE_LIMIT);
 
 const limit = async ()=>{
-    results = await searchModel.collection.stats( { scale : 1024 } )
-    return results.size < STORAGE_LIMIT
+    if(STORAGE_LIMIT > 0){
+        const results = await searchModel.collection.stats( { scale : 1024 } );
+        return results.size < STORAGE_LIMIT;
+    } else {
+        return true;
+    }
 }; 
 
 module.exports = { limit };
